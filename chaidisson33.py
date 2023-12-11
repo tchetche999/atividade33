a98e0a5acb874c468e722a4099276c262fd057eb
tchetche999 <147431636+tchetche999@users.noreply.github.com>
2023-12-11 14:26:26 -0300

    Add files via upload

---
+++ chaidisson33.py
@@ -0,0 +1,10 @@
+def acoes(a, b):
+    soma = a+b
+    subtra = a - b
+    multpli = a*b
+    div = a/b
+    print(soma,subtra,multpli,div)
+
+n1= int(input("Digite um número: "))
+n2= int(input('Digite um número: '))
+acoes(n1, n2)
